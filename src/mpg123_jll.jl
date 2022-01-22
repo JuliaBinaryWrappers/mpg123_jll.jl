64e7a490533d4b88662597aef84c499f88e2ac55
jlbuild <juliabuildbot@gmail.com>
2022-01-22 22:20:31 +0000

    mpg123_jll build 1.29.3+0

--- src/mpg123_jll.jl
+++ src/mpg123_jll.jl
@@ -1,31 +1,9 @@
-module mpg123_jll
-using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
-import Base: UUID
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule mpg123_jll
+using Base
+using Base: UUID
+import JLLWrappers
 
-# We put these inter-JLL-package API values here so that they are always defined, even if there
-# is no underlying wrapper held within this JLL package.
-const PATH_list = String[]
-const LIBPATH_list = String[]
-
-# Load Artifacts.toml file
-artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
-
-# Extract all platforms
-artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("3205ef68-7822-558b-ad0d-1b4740f12437"))
-platforms = [Pkg.Artifacts.unpack_platform(e, "mpg123", artifacts_toml) for e in artifacts["mpg123"]]
-
-# Filter platforms based on what wrappers we've generated on-disk
-platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
-
-# From the available options, choose the best platform
-best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
-
-# Silently fail if there's no binaries for this platform
-if best_platform === nothing
-    @debug("Unable to load mpg123; unsupported platform $(triplet(platform_key_abi()))")
-else
-    # Load the appropriate wrapper
-    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
-end
-
+JLLWrappers.@generate_main_file_header("mpg123")
+JLLWrappers.@generate_main_file("mpg123", UUID("3205ef68-7822-558b-ad0d-1b4740f12437"))
 end  # module mpg123_jll
